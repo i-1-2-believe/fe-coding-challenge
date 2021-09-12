a154ec195eb5a399bbb14fe9914f10a04f6ea7dc
Junaid Mohammed <339900+i-1-2-believe@users.noreply.github.com>
2021-09-13 00:16:15 +0100

    Create tic-tac-toe board

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -6,23 +6,40 @@ import { selectCell } from '../../store/actions/moves';
 const selectBoard = (state) => state.board
 const selectGame = (state) => state.game
 
-const getRandomCell = (i) => Math.floor(Math.random() * i)
-
 export const Board = () => {
   const board = useSelector(selectBoard)
   const game = useSelector(selectGame)
   const dispatch = useDispatch()
 
+  function Tile(props) {
+    return (
+      <div 
+        className="tile" 
+        onClick={() => dispatch(
+          selectCell(
+            game.currentPlayer,
+            props.x,
+            props.y
+          )
+        )}
+      >
+        <div>{props.player}</div>
+      </div>
+    );
+  };
+
   return (
     <div className="Board">
-      Board: { JSON.stringify(board) }
-      <div onClick={() => dispatch(
-        selectCell(
-          game.currentPlayer,
-          getRandomCell(board.length),
-          getRandomCell(board.length)
-        )
-      )}>Player {game.currentPlayer}</div>
+      <Tile x="0" y="0" player={board[0][0]} />
+      <Tile x="0" y="1" player={board[0][1]} />
+      <Tile x="0" y="2" player={board[0][2]} />
+      <Tile x="1" y="0" player={board[1][0]} />
+      <Tile x="1" y="1" player={board[1][1]} />
+      <Tile x="1" y="2" player={board[1][2]} />
+      <Tile x="2" y="0" player={board[2][0]} />
+      <Tile x="2" y="1" player={board[2][1]} />
+      <Tile x="2" y="2" player={board[2][2]} />
+      <div>Player {game.currentPlayer}</div>
     </div>
   )
 }
